2b03f088aafe30bc5f891c9764b029f92399faa1
yeongjuning <yeongjuning@gmail.com>
2022-04-30 15:44:25 +0900

    Project Commit
    3d Test Project Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public enum EButtonType
+    {
+        MoveToWards,
+        RotateWards,
+
+        Max,
+    }
+    
+    public static GameManager Manager { private set; get; }
+    public EButtonType eButtonType { private get; set; }
+
+    [SerializeField] Button[] buttons_;
+    
+    bool[] isClicked = new bool[(int)EButtonType.Max];
+    
+    private void Awake()
+    {
+        Manager = this;
+    }
+    
+    public void OnClickButton(int idx)
+    {
+        eButtonType = (EButtonType)idx;
+        switch ((EButtonType)idx)
+        {
+            case EButtonType.MoveToWards:
+            case EButtonType.RotateWards:
+                isClicked[idx] = true;
+                break;
+        }
+    }
+
+    public bool IsClicked(int idx)
+    {
+        for (int i = 0; i < (int)EButtonType.Max; i++)
+        {
+            isClicked[i] = false;
+        }
+
+        return isClicked[idx] = true;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Player : MonoBehaviour
+{
+    [SerializeField] GameManager gameManager_;
+    [SerializeField] GameObject target_;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (null != gameManager_)
+        {
+        }
+    }
+
+
+}
